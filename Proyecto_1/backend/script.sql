3958ae26719798c4023deedc788ad309bcddea88
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2024-03-18 01:04:22 -0600

    Update script.sql

--- Proyecto_1/backend/script.sql
+++ Proyecto_1/backend/script.sql
@@ -1,9 +1,76 @@
+CREATE TABLE `paises` (
+  `id_pais` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_pais`));
 
-SELECT * FROM bdmysql.paises;
+
+CREATE TABLE `categorias` (
+  `id_categoria` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_categoria`));
+
+
+CREATE TABLE `clientes` (
+`id_cliente` INT NOT NULL,
+`Nombre` VARCHAR(45) NOT NULL,
+`Apellido` VARCHAR(45) NOT NULL,
+`Direccion` VARCHAR(45) NOT NULL,
+`Telefono` VARCHAR(10) NOT NULL,
+`Tarjeta` BIGINT NOT NULL,
+`Edad` INT NOT NULL,
+`Salario` INT NOT NULL,
+`Genero` CHAR(1) NOT NULL,
+`id_pais` INT NOT NULL,
+
+PRIMARY KEY (`id_cliente`));
+
+
+
+
+
+
+  CREATE TABLE `detalle_orden` (
+  `id_detalle_orden` INT NOT NULL,
+  `id_orden` INT NOT NULL,
+  `linea_orden` INT NOT NULL,
+  `id_vendedor` INT NOT NULL,
+  `id_producto` INT NOT NULL,
+  `cantidad` INT NOT NULL,
+
+  PRIMARY KEY (`id_detalle_orden`));
+
+
+  CREATE TABLE `orden` (
+  `id_orden` INT NOT NULL,
+  `fecha_orden` DATE NOT NULL,
+  `id_cliente` INT NOT NULL,
+  
+  PRIMARY KEY (`id_orden`));
+
+
+
+
+  CREATE TABLE `productos` (
+  `id_producto` INT NOT NULL,
+  `Nombres` VARCHAR(45) NOT NULL,
+  `Precio` FLOAT NOT NULL,
+  `id_categoria` INT NOT NULL,
+  
+  PRIMARY KEY (`id_producto`));
+
+
+
+  CREATE TABLE `vendedores` (
+  `id_vendedor` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `id_pais` INT NOT NULL,
+  
+  PRIMARY KEY (`id_vendedor`));
+
+
+
 TRUNCATE bdmysql.clientes;
-
 TRUNCATE bdmysql.paises;
-
 TRUNCATE bdmysql.orden;
 TRUNCATE bdmysql.productos;
 TRUNCATE bdmysql.vendedores;
